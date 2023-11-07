49d49ab90086554ca857f2911d96f5b0bcb68dba
Regulus0811 <kbc6268@g.yju.ac.kr>
2023-11-07 17:24:04 +0900

    feat: Add logout button

--- resources/views/mainpage/header.blade.php
+++ resources/views/mainpage/header.blade.php
@@ -22,20 +22,30 @@ header {
             background-color: #39c5bb !important;
         }
         </style>
-   <nav class="navbar bg-body-tertiary fixed-top">
+   <nav class="navbar fixed-top">
   <div class="container-fluid">
     <img class="logo" src="{{asset('image/music.svg')}}" alt="logo">
-    <a class="navbar-brand" href="/">VocaHub</a>
+    <a class="navbar-brand" href="/"><h1>VocaHub</h1></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" data-bs-backdrop="false">
             <div class="offcanvas-header">
-              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">VocaHub</h5>
+              <h3 class="offcanvas-title" id="offcanvasNavbarLabel">VocaHub</h3>
               <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    @guest
+                    <a class="nav-link active" aria-current="page" href="/login">
+                        로그인
+                    </a>
+                    @endguest
+                    @auth
+                        <li class="nav-item me-3">
+                        <p>환영합니다{{ Auth::user()->name }}님!</p>
+                    </li>
+                    @endauth
                     <li class="nav-item me-3">
                         <a class="nav-link active" aria-current="page" href="/">홈페이지 소개</a>
                     </li>
@@ -55,24 +65,28 @@ header {
                         </a>
                     </li>
                     <li class="nav-item dropdown me-3">
-                        @guest
-                        <a class="nav-link active" aria-current="page" href="/login">
-                            로그인
-                        </a>
-                        @endguest
                         @auth
-                        <a class="nav-link active" aria-current="page" href="/logout">
-                            로그아웃
+                        <a class="nav-link active" aria-current="page" href="/dashboard">
+                            내정보
                         </a>
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-responsive-nav-link :href="route('logout')"
+                                                    onclick="event.preventDefault();
+                                                                this.closest('form').submit();">
+                                {{ __('로그아웃') }}
+                            </x-responsive-nav-link>
+                        </form>
+
                         @endauth
                     </li>
 
                 </ul>
 
-                <form class="d-flex" role="search">
+                {{-- <form class="d-flex" role="search">
                     <input class="form-control me-2" type="search" placeholder="검색" aria-label="Search">
                     <button class="btn btn-warning" type="submit">Search</button>
-                </form>
+                </form> --}}
             </div>
           </div>
         </div>
@@ -82,5 +96,3 @@ header {
 {{-- dropdown 의존성을 위한 jquery와 popper.js 주입 --}}
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-
-
